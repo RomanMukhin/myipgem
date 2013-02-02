ea24e74ad168c7b5dc35f4ddd6ee5bba3e14500b
Roman Mukhin <roman11mukhin@gmail.com>
2013-02-02 21:07:25 +0200

    Update spec/myip_spec.rb

--- spec/myip_spec.rb
+++ spec/myip_spec.rb
@@ -1,3 +1,3 @@
+require 'webmock/rspec'
 require File.dirname(__FILE__) + './../lib/myip.rb'
 require File.dirname(__FILE__) + '/myip/myip_test.rb'
-require File.dirname(__FILE__) + './../lib/myip/command.rb'
